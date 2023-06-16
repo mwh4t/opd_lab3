1567a7caaafb8636fc2b61ae815ab11dca3a97e6
mwh4t <korzunov.matvey@yandex.ru>
2023-06-16 22:55:34 +0300

    first commit

---
+++ opd_lab3_1/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Вариант 13.
+Даны действительные числа X, Y, Z. Если сумма чисел меньше 1, то 
+наименьшее из них заменить суммой квадратов двух других. В противном 
+случае значения X, Y, Z заменить полусуммой двух других.
+*/
+using System;
+
+namespace opd_lab_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3 действительных числа:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            double sum = x + y + z;
+            double min = Math.Min(Math.Min(x, y), z);
+
+            double halfX = (y + z) / 2;
+            double halfY = (x + z) / 2;
+            double halfZ = (x + y) / 2;
+
+            if (sum < 1)
+            {
+                x = (min == x) ? y * y + z * z : x;
+                y = (min == y) ? x * x + z * z : y;
+                z = (min == z) ? x * x + y * y : z;
+            }
+            else
+            {
+                x = halfX;
+                y = halfY;
+                z = halfZ;
+            }
+
+            Console.WriteLine($"Обновлённые числа: X = {x}, Y = {y}, Z = {z}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ opd_lab3_2/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Вариант 13.
+По введённому номеру единицы измерения (1 - дециметр,
+2 - километр, 3 - метр, 4 - миллиметр, 5 - сантиметр) и длине
+отрезка L получить соответствующее значение длины отрезка в
+метрах.
+*/
+using System;
+
+namespace opd_lab3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Единицы измерения:");
+            Console.WriteLine("1 - Дециметр");
+            Console.WriteLine("2 - Километр");
+            Console.WriteLine("3 - Метр");
+            Console.WriteLine("4 - Миллиметр");
+            Console.WriteLine("5 - Сантиметр");
+
+            Console.Write("Выберите число: ");
+            int chislo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите длину: ");
+            double dlina = Convert.ToDouble(Console.ReadLine());
+
+            double dlinaVmetrah = 0;
+
+            switch (chislo)
+            {
+                case 1: // Дециметры в метры
+                    dlinaVmetrah = dlina / 10;
+                    break;
+                case 2: // Километры в метры
+                    dlinaVmetrah = dlina * 1000;
+                    break;
+                case 3: // Метры
+                    dlinaVmetrah = dlina;
+                    break;
+                case 4: // Миллиметры в метры
+                    dlinaVmetrah = dlina / 1000;
+                    break;
+                case 5: // Сантиметры в метры
+                    dlinaVmetrah = dlina / 100;
+                    break;
+                default:
+                    Console.WriteLine("Что-то пошло не так :(");
+                    return;
+            }
+
+            Console.WriteLine($"Длина в метрах: {dlinaVmetrah} м");
+            Console.ReadKey();
+        }
+    }
+}
